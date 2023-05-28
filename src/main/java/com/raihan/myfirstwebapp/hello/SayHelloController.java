0255c9cb7569b0819a2373fd6e24b8459b643ee5
Raihan Evan <raihan.evan@gmail.com>
2023-05-28 22:07:40 +0600

    stringbuffer append

--- src/main/java/com/raihan/myfirstwebapp/hello/SayHelloController.java
+++ src/main/java/com/raihan/myfirstwebapp/hello/SayHelloController.java
@@ -5,12 +5,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 
-@Controller
+@Controller //is a controller/web api class
 public class SayHelloController {
-    @RequestMapping("say-hello")
-    @ResponseBody
+    @RequestMapping("say-hello")    //url mapping to the link
+    @ResponseBody       //string repesent
     public String sayHello(){
         return "Hello! this is a hello page.:v";
+    }
+    @RequestMapping("say-hello-html1")    //url mapping to the link
+    @ResponseBody       //string repesent
+    public String sayHelloHTML1(){
+        StringBuffer stringBuffer = new StringBuffer();
+        stringBuffer.append("<html lang=\"en\">\n" +
+                "<head>\n" +
+                "    <meta charset=\"UTF-8\">\n" +
+                "    <title>My First HTML Page</title>\n" +
+                "</head>\n" +
+                "<body>\n" +
+                "    Looking for Something???\n" +
+                "</body>\n" +
+                "</html>");
+        return stringBuffer.toString();
     }
 
 }

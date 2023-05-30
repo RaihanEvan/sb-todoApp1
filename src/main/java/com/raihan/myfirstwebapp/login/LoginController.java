e36904e41d73d22ccf38efa15d53996fc4dbe99a
Raihan Evan <raihan.evan@gmail.com>
2023-05-30 18:35:06 +0600

    logger added

--- src/main/java/com/raihan/myfirstwebapp/login/LoginController.java
+++ src/main/java/com/raihan/myfirstwebapp/login/LoginController.java
@@ -1,5 +1,7 @@
 package com.raihan.myfirstwebapp.login;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,9 +14,12 @@ public class LoginController {
 //		return "login";
 //		
 //	}
+	private Logger logger = LoggerFactory.getLogger(getClass());
+	
 	@RequestMapping("login")
 	public String gotoLoginPage(@RequestParam String name, ModelMap model) {
 		model.put("name", name);
+		logger.debug("Req param is "+name);
 		System.out.println("Request param is "+name);
 		return "login";
 	}
